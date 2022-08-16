cff5c6cfc8178cd0d301c03be0ed5d5e147ecc35
alefay2022 <alefay2022@105SE32-7R0TVD3.ad.campus-eni.fr>
2022-08-16 17:03:44 +0200

    Mise à jour BO avec l'intégralité des variables, getter et setter. + un avec seulement id, pseudo, motDePasse

--- src/main/java/bo/Utilisateurs.java
+++ src/main/java/bo/Utilisateurs.java
@@ -4,20 +4,58 @@ public class Utilisateurs {
 
 	int id;
 	String pseudo;
+	String nom;
+	String prenom;
+	String email;
+	String telephone;
+	String rue;
+	String codePostal;
+	String ville;
 	String motDePasse;
-	
+	int credit;
+	byte admin;
+
+	public Utilisateurs(int id, String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			String codePostal, String ville, String motDePasse, int credit, byte admin) {
+		super();
+		this.id = id;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.admin = admin;
+	}
+
+	public Utilisateurs(String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			String codePostal, String ville, String motDePasse, int credit, byte admin) {
+		super();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.admin = admin;
+	}
+
+
 	public Utilisateurs(int id, String pseudo, String motDePasse) {
 		this.id = id;
 		this.pseudo = pseudo;
 		this.motDePasse = motDePasse;
 	}
-	
-	public Utilisateurs(String pseudo, String motDePasse)  {
-		this.pseudo = pseudo;
-		this.motDePasse = motDePasse;
-	}
-	
-	public Utilisateurs()  {
+
+	public Utilisateurs() {
 
 	}
 
@@ -37,6 +75,62 @@ public class Utilisateurs {
 		this.pseudo = pseudo;
 	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
 	public String getMotDePasse() {
 		return motDePasse;
 	}
@@ -45,10 +139,26 @@ public class Utilisateurs {
 		this.motDePasse = motDePasse;
 	}
 
+	public int getCredit() {
+		return credit;
+	}
 
-	
-	
-	
-	
-	
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public byte getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(byte admin) {
+		this.admin = admin;
+	}
+
+
+
+
+
+
+
 }
