465f3508aa71641045e8ed289a173872e8c985b7
ndelhayeboloh2022 <ndelhayeboloh2022@105SE32-FS0TVD3.ad.campus-eni.fr>
2022-08-16 17:21:52 +0200

    implémentation dans la DAL de la fonction Insert (si ça marche pas c'est la faute à lohann avec sa requete de merde là)

--- src/main/java/dal/UtilisateursDAOJdbcImp.java
+++ src/main/java/dal/UtilisateursDAOJdbcImp.java
@@ -12,7 +12,8 @@ import bo.Utilisateurs;
 
 public class UtilisateursDAOJdbcImp  {
 
-
+	private final String INSERT = "insert into utilisateurs (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, "
+			+ "mot_de_passe, credit, administrateur)VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 
 	public Utilisateurs getUtilisateurByMailMDP(String pseudo, String mdp) throws DALException{
@@ -58,6 +59,37 @@ public class UtilisateursDAOJdbcImp  {
 			}
 		}
 		return utilisateur;
+	}
+	
+	public Utilisateurs insert(Utilisateurs u) throws DALException  {
+		try (Connection con = JdbcTools.getConnection();
+				PreparedStatement stmt = con.prepareStatement(INSERT)){
+			try {
+				con.setAutoCommit(false);
+				
+				stmt.setString(1, u.getPseudo());
+				stmt.setString(2, u.getNom());
+				stmt.setString(3, u.getPrenom());
+				stmt.setString(4, u.getEmail());
+				stmt.setString(5, u.getTelephone());
+				stmt.setString(6, u.getRue());
+				stmt.setString(7, u.getCodePostal());
+				stmt.setString(8, u.getVille());
+				stmt.setString(9, u.getMotDePasse());
+				stmt.setInt(10, u.getCredit());
+				stmt.setByte(11, u.getAdmin());
+				
+				stmt.executeUpdate();
+				
+				con.commit();
+			}catch(SQLException e){
+				con.rollback();	
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			throw new DALException("Erreur lors de l'insert : " + e.getMessage());
+		}
+		return u;
 	}
 
 
