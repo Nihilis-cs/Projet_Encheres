17878cb8b24857e10f3339dd968a63dc7b597097
ndelhayeboloh2022 <ndelhayeboloh2022@105SE32-FS0TVD3.ad.campus-eni.fr>
2022-08-17 15:51:29 +0200

    PROUUUUUUUUUT

--- src/main/java/servlets/GestionProfil.java
+++ src/main/java/servlets/GestionProfil.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class GestionProfil
  */
-@WebServlet("/gestion/profil")
+@WebServlet("/utilisateur/gestion")
 public class GestionProfil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
