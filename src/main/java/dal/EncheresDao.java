c5c39e9957733f0414f09c745c3a92308dfddd84
alefay2022 <alefay2022@105SE32-7R0TVD3.ad.campus-eni.fr>
2022-08-18 14:17:08 +0200

    commit pour récup les changement de yohan

--- src/main/java/dal/EncheresDao.java
+++ src/main/java/dal/EncheresDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import bo.Encheres;
 
 public interface EncheresDao {
-	public Encheres insert(Encheres e);
+	public Encheres insert(Encheres e) throws DALException;
 	public List<Encheres> selectAll();
 	public Encheres selectByNoArticle();
 }
