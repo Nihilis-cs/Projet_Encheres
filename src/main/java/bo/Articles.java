f6a42cda4f2c5ca7a3117b2e8dd26ab49410833f
ndelhayeboloh2022 <ndelhayeboloh2022@105SE32-FS0TVD3.ad.campus-eni.fr>
2022-08-18 09:39:27 +0200

    bo mise à jour

---
+++ src/main/java/bo/Articles.java
@@ -0,0 +1,20 @@
+package bo;
+
+import java.time.LocalDateTime;
+
+public class Articles {
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDateTime dateDebutEnchere;
+	private LocalDateTime dateFinEnchere;
+	private int prixInitial;
+	private int prixVente;
+	private Utilisateurs vendeur;
+	private Categories categorie;
+	private String etatVente;
+	private String image;
+	
+	
+	
+}
