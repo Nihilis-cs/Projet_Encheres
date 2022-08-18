afe7ed495e271c1e427d74ba8af3a571f1524f97
ndelhayeboloh2022 <ndelhayeboloh2022@105SE32-FS0TVD3.ad.campus-eni.fr>
2022-08-18 11:40:43 +0200

    modifs sur Articles et Encheres

--- src/main/java/bo/Encheres.java
+++ src/main/java/bo/Encheres.java
@@ -8,4 +8,41 @@ public class Encheres {
 	private LocalDateTime dateEnchere;
 	private int montantEnchere;
 	
+	public Encheres(Utilisateurs encherisseur, int noArticle, LocalDateTime dateEnchere, int montantEnchere) {
+		this.encherisseur = encherisseur;
+		this.noArticle = noArticle;
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+	}
+	
+	public Encheres() {
+
+	}
+	
+	public Utilisateurs getEncherisseur() {
+		return encherisseur;
+	}
+	public void setEncherisseur(Utilisateurs encherisseur) {
+		this.encherisseur = encherisseur;
+	}
+	public int getNoArticle() {
+		return noArticle;
+	}
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+	public LocalDateTime getDateEnchere() {
+		return dateEnchere;
+	}
+	public void setDateEnchere(LocalDateTime dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+	public int getMontantEnchere() {
+		return montantEnchere;
+	}
+	public void setMontantEnchere(int montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+	
+	
 }
