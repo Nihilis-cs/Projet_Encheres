768787f303a804107867168e54484dbc18f13110
alefay2022 <alefay2022@105SE32-7R0TVD3.ad.campus-eni.fr>
2022-08-18 13:52:27 +0200

    Ras le cul des commits avec votre giteub de merde

--- src/main/java/bll/EncheresManager.java
+++ src/main/java/bll/EncheresManager.java
@@ -1,5 +1,21 @@
 package bll;
 
+import dal.DAOFactory;
+import dal.EncheresDao;
+
 public class EncheresManager {
+	private static EncheresManager enchMngr;
+	private EncheresDao encheresDao;	
+	
+	private EncheresManager() {
+		this.encheresDao = DAOFactory.getEnchereDao();
+	}
+	
+	public static EncheresManager getInstance() {
+		if(enchMngr == null) {
+			enchMngr = new EncheresManager();
+		}
+		return enchMngr;
+	}
 
 }
