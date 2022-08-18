a2dc91293c23d336454c84b0caa37d9ac6188c09
ndelhayeboloh2022 <ndelhayeboloh2022@105SE32-FS0TVD3.ad.campus-eni.fr>
2022-08-18 14:30:31 +0200

    retirage d'une methode inutile dans EncheresDAO

--- src/main/java/dal/EncheresDao.java
+++ src/main/java/dal/EncheresDao.java
@@ -5,7 +5,6 @@ import java.util.List;
 import bo.Encheres;
 
 public interface EncheresDao {
-	public Encheres insert(Encheres e);
-	public List<Encheres> selectAll();
+	public Encheres insert(Encheres e) throws DALException;
 	public Encheres selectByNoArticle();
 }
