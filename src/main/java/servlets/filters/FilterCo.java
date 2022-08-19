c02e0f247495de276aaa7bfafe15be0421c809f8
ydoucet2022 <ydoucet2022@105SE32-3RBVVD3.ad.campus-eni.fr>
2022-08-19 14:23:41 +0200

    filter filterey pouet pouet

--- src/main/java/servlets/filters/FilterCo.java
+++ src/main/java/servlets/filters/FilterCo.java
@@ -16,40 +16,38 @@ import javax.servlet.http.HttpServletRequest;
 import bo.Utilisateurs;
 
 /**
- * Servlet Filter implementation class FilterAdmin
+ * Servlet Filter implementation class FilterCo
  */
 @WebFilter(dispatcherTypes = {
 				DispatcherType.REQUEST, 
 				DispatcherType.FORWARD, 
 				DispatcherType.INCLUDE
 		}
-					, urlPatterns = { "/administrateur/*" })
-public class FilterAdmin extends HttpFilter implements Filter {
+					, urlPatterns = { "/navigation/vente", "/enchere/proposer" })
+public class FilterCo extends HttpFilter implements Filter {
        
-    
-    public FilterAdmin() {
+
+    public FilterCo() {
         super();
         // TODO Auto-generated constructor stub
     }
-
 
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
-
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// cast de la demmande de l'utilisateur
+
+
 		HttpServletRequest httpRequest = (HttpServletRequest)request;
 		Utilisateurs u = (Utilisateurs) httpRequest.getSession().getAttribute("utilisateurActif");
-		System.out.println("Passage in da filtah");
-		if (u.getAdmin() == 1) {
+		if (u != null) {
 			chain.doFilter(request, response);
-		}else {
-			RequestDispatcher rd = httpRequest.getRequestDispatcher("");
+		} else {
+			RequestDispatcher rd = httpRequest.getRequestDispatcher("/WEB-INF/login.jsp");
 			rd.forward(httpRequest, response);
 		}
-			
+		
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
