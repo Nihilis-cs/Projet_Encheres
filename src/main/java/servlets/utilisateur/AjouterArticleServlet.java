c743938ed61f9edc90e1b1aab7bf943691a70c78
ydoucet2022 <ydoucet2022@105SE32-3RBVVD3.ad.campus-eni.fr>
2022-08-19 09:33:45 +0200

    sa avance un peu tavu

--- src/main/java/servlets/utilisateur/AjouterArticleServlet.java
+++ src/main/java/servlets/utilisateur/AjouterArticleServlet.java
@@ -1,16 +1,20 @@
 package servlets.utilisateur;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import bll.UtilisateursManager;
 import bo.Articles;
+import bo.EtatsVente;
 
 /**
  * Servlet implementation class AjouterArticleServlet
@@ -33,6 +37,9 @@ public class AjouterArticleServlet extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session  =request.getSession();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+		
 		//Recuperation des données de l'article
 		String nomArticle = request.getParameter("nomArticle");
 		String categorie = request.getParameter("categorie");
@@ -42,9 +49,14 @@ public class AjouterArticleServlet extends HttpServlet {
 		String prixInit = request.getParameter("prixInit");
 		System.out.println(nomArticle + categorie + description + dateDebut + dateFin + prixInit);
 		
+		//convert String to LocalDate
+		LocalDate dateDebutParse = LocalDate.parse(dateDebut, formatter);
+		LocalDate dateFinParse = LocalDate.parse(dateFin, formatter);
+
+			
 		UtilisateursManager um =UtilisateursManager.getInstance();
 		
-//		Articles article = new Articles(nomArticle, categorie, description, dateDebut, dateFin, prixInit);
+//		Articles article = new Articles(nomArticle, description, dateDebutParse, dateFinParse, prixInit, null,  session.getAttribute("utilisateurActif"),null, EtatsVente.CR, null)   
 	}
 
 }
