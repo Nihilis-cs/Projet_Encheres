b60b947112ded94ae38afae5c23f422cf67aa0d0
ndelhayeboloh2022 <ndelhayeboloh2022@105SE32-FS0TVD3.ad.campus-eni.fr>
2022-08-24 10:49:23 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/Nihilix420/ENI_ENCHERES.git
    into main

--- src/main/java/servlets/article/FiltrerServlet.java
+++ src/main/java/servlets/article/FiltrerServlet.java
@@ -23,14 +23,14 @@ import bo.Utilisateurs;
 @WebServlet("/article/filtrer")
 public class FiltrerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FiltrerServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FiltrerServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -45,43 +45,57 @@ public class FiltrerServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
+		ArticlesManager am =ArticlesManager.getInstance();
 		Utilisateurs u = (Utilisateurs) session.getAttribute("utilisateurActif");
-		int idUser = u.getId();
-		String achatVente = request.getParameter("type-encheres");
+		String requete = null;
+		int idUser; //= u.getId();
+		
+		String idCategorieStr = request.getParameter("categorie");
+		System.out.println(idCategorieStr);
+		int idCategorie = Integer.parseInt(idCategorieStr);
+		
 		String articleContenant = request.getParameter("article-contenant");
 		System.out.println(articleContenant);
-		//String categorie = request.getParameter("categorie");
-		String requete = null;
-		//System.out.println(achatVente);
-		ArticlesManager am =ArticlesManager.getInstance();
-		
-				
-		if(achatVente.equals("achats")) {
-			String enchere1 = request.getParameter("encheres1");
-			String enchere2 = request.getParameter("encheres2");
-			String enchere3 = request.getParameter("encheres3");
-			requete = achatVente + enchere1 + enchere2 + enchere3;
-			//System.out.println(requete);
-		} else if (achatVente.equals("ventes")) {
-			String vente1 = request.getParameter("ventes1");
-			String vente2 = request.getParameter("ventes2");
-			String vente3 = request.getParameter("ventes3");
-			requete = achatVente + vente1 + vente2 + vente3;
-			//System.out.println(requete);
-		}
+		if (u != null) {
+			idUser = u.getId();
+			String achatVente = request.getParameter("type-encheres");
 
-			String idCategorieStr=request.getParameter("categorie");
-			System.out.println(idCategorieStr);
-			int idCategorie = Integer.parseInt(idCategorieStr);
+			if(achatVente.equals("achats")) {
+				String enchere1 = request.getParameter("encheres1");
+				String enchere2 = request.getParameter("encheres2");
+				String enchere3 = request.getParameter("encheres3");
+				requete = achatVente + enchere1 + enchere2 + enchere3;
+				//System.out.println(requete);
+			} else if (achatVente.equals("ventes")) {
+				String vente1 = request.getParameter("ventes1");
+				String vente2 = request.getParameter("ventes2");
+				String vente3 = request.getParameter("ventes3");
+				requete = achatVente + vente1 + vente2 + vente3;
+				//System.out.println(requete);
+			}
+
 			
-		System.out.println(requete+idCategorie+articleContenant);
-		//System.out.println(requete+idUser);
-		try {
-			List<Articles> liste = am.selectAllFilter(requete, idUser, idCategorie, articleContenant);
-			request.setAttribute("liste", liste);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+
+			System.out.println(requete+idCategorie+articleContenant);
+			//System.out.println(requete+idUser);
+			try {
+				
+				List<Articles> liste = am.selectAllFilter(requete, idUser, idCategorie, articleContenant);
+				request.setAttribute("liste", liste);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
+			}
+		}else {
+			try {
+				requete = "";
+				System.out.println("En mode deco la requete est : " +requete);
+				List<Articles> liste = am.selectAllFilter(requete, 0, idCategorie, articleContenant);
+				request.setAttribute("liste", liste);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		RequestDispatcher rs = request.getRequestDispatcher("/WEB-INF/index.jsp");
 		rs.forward(request, response);
